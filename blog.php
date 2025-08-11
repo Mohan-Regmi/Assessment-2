e894ae3d6e15b33661c4e442826edbbb2df2975e
Mohan-Regmi <987871@win.edu.au>
2025-08-11 10:19:53 +1000

    pages

---
+++ blog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My page</title>
+</head>
+<body>
+<?php
+$name ="Sai Lakkaraju";
+$firstName = substr($name,0,3);
+echo "my first name is $firstName";
+?>
+</body>
+</html>
